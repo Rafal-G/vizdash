9c55a35540724b6c64579faa6dc6363b981490a3
Rafal Gostynski <rgostynski@gmail.com>
2017-02-18 09:44:39 +1300

    Initial commit from Yeoman angular template

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vizdashApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vizdashApp
+ */
+angular.module('vizdashApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vizdashApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vizdashApp
+ */
+angular.module('vizdashApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
