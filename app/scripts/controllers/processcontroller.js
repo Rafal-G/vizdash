4bd6c7fe51d28be166affb627b45851af8b53c72
Rafal Gostynski <rgostynski@gmail.com>
2017-02-18 20:52:43 +1300

    Process Memory update

--- app/scripts/controllers/processcontroller.js
+++ app/scripts/controllers/processcontroller.js
@@ -9,21 +9,26 @@
 angular.module('vizdashApp')
   .controller('ProcessController', ['$scope', '$timeout', 'ProcessService',function($scope, $timeout, ProcessService) {
     $scope.count = 0;
+    $scope.processStatus= "On";
     $scope.cpuUsage = 0;
+    $scope.memoryUsage = 0;
     $scope.fadedIn = true;
-    $scope.addCounter = function() {
-      console.log("Counter clicked");
-      $scope.count++;
-    };
+    $scope.instances = Math.floor((Math.random() * 10) + 1);
+
     $scope.processCount = ProcessService.getProcessCount();
 
     $scope.upTime = 0;
-    var updateTimer = function() {
+    var ticker = function() {
       $scope.upTime++;
       $timeout(updateCPU, 1000);
-      $timeout(updateTimer, 1000);
+      $timeout(updateMemory, 1500);
+      $timeout(ticker, 1000);
     };
-    updateTimer();
+    ticker();
+
+    var updateMemory = function() {
+      $scope.memoryUsage = ProcessService.getProcessMemory($scope.memoryUsage)
+    };
     var updateCPU = function() {
       $scope.cpuUsage = Math.floor((Math.random() * 100) + 1);
     }
--- app/scripts/services/processservice.js
+++ app/scripts/services/processservice.js
@@ -4,14 +4,33 @@
  * @ngdoc service
  * @name vizdashApp.processService
  * @description
- * # podService
+ * # processService
  * Service in the vizdashApp.
  */
 angular.module('vizdashApp')
   .service('ProcessService', function () {
     var processCount = 0;
+    
+    var subtract = function (x, y) {
+      return x - y;
+    };
+
+    var add = function (x, y) {
+      return x + y;
+    };
+
+    var modifyAmount = function (amount, operatorFunction) {
+      return operatorFunction(amount, Math.floor((Math.random() * 10) + 1));
+    };
 
     return {
+      getProcessMemory: function (amount) {
+        if (amount >= 50) {
+          return modifyAmount(amount, subtract);
+        } else {
+          return modifyAmount(amount, add);
+        }
+      },
       getProcessCount: function () {
         return processCount;
       },
